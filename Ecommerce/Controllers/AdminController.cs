178ece32a686dce11542b0c2f1c62ffe268a02fd
TrumpSsta <jedrzeej97@gmail.com>
2020-06-11 23:34:12 +0200

    Table added to Categories Page

--- Ecommerce/Controllers/AdminController.cs
+++ Ecommerce/Controllers/AdminController.cs
@@ -1,17 +1,29 @@
-﻿using System;
+﻿using Ecommerce.DAL;
+using Ecommerce.Repository;
+using OnlineShoppingStore.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Ecommerce.Controllers
+namespace OnlineShoppingStore.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
         public ActionResult Dashboard()
         {
             return View();
+        }
+
+
+        public ActionResult Categories()
+        {
+            List<Category> allcategories = _unitOfWork.GetRepositoryInstance<Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allcategories);
         }
     }
 }
