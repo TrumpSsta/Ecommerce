2b50dbebe38f2717ede2cbe96ff1436c39b27d95
TrumpSsta <jedrzeej97@gmail.com>
2020-06-19 14:55:52 +0200

    SHoping cart created
    Shopping cart works, can add and delete products, ifxing not loading images

--- Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Ecommerce.Models.Home;
+﻿using Ecommerce.DAL;
+using Ecommerce.Models.Home;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace Ecommerce.Controllers
 {
     public class HomeController : Controller
     {
+        ecommerceDBEntities ctx = new ecommerceDBEntities();
 
         public ActionResult Index(string search, int? page)
         {
@@ -21,6 +23,63 @@ namespace Ecommerce.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+        public ActionResult AddToCart(int productId)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = ctx.Products.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Products.Find(productId);
+                foreach (var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int prevQty = item.Quantity;
+                        cart.Remove(item);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + 1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = 1
+                        });
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect("Index");
+        }
+        public ActionResult RemoveFromCart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+            Session["cart"] = cart;
+            return Redirect("Index");
         }
 
         public ActionResult Contact()
---
+++ Ecommerce/Models/Home/Item.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models.Home
+{
+    public class Item
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
